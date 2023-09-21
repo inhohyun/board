22c2258df59bfedf941af5a7a3170939d2a1158a
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-09-21 23:47:59 +0900

    프로젝트 중단

--- board/src/main/java/com/example/board/controller/ArticleController.java
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
-
+// 프로젝트 중단, 프리코스 후 다시 돌아올 것
 @Controller
 public class ArticleController {
     // new로 객체를 생성하는 것이 아닌 스프링 부트가 미리 생성해 놓은 객체를 가져다가 자동연결
@@ -66,4 +66,43 @@ public class ArticleController {
 
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    private String edit(@PathVariable Long id, Model model) {
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        return "articles/edit";
+
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        //1. dto를 엔티티로 변환한다.
+        Article articleEntity = form.toEntity();
+        //2. 엔티티를 db로 저장한다.
+        //2-1. db에 기존 데이터를 가져온다.
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        //2-2. 기존 데이터가 있다면 값을 갱신한다.
+        if (target != null) {
+            articleRepository.save(articleEntity); // form에서 받아온 데이터로 저장
+        }
+        //3. 수정 결과 페이지로 리다이렉트 한다.
+        return "redirect:" + articleEntity.getId();
+    }
+
+    @GetMapping("articles/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        //1. 삭제 대상을 가져온다.
+        Article target = articleRepository.findById(id).orElse(null);
+        //2. 대상을 삭제한다.
+        if (target != null) {
+            articleRepository.delete(target);
+        }
+        //3. 결과 페이지로 리다이렉트 한다.
+        return "/articles";
+    }
+
 }
